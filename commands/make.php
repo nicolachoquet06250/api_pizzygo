29ab98955cd9fc0c01ef1dd34a5c39ead819433e
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-01-23 15:42:50 +0100

    add demo and error controller

--- commands/make.php
+++ commands/make.php
@@ -1,5 +1,43 @@
 <?php
 
 class make extends cmd {
-
+	/**
+	 * @return bool|string
+	 * @throws \PHPMailer\PHPMailer\Exception
+	 */
+	protected function send_sms() {
+		$sPhoneNum = '+33763207630'; // Le numéro de téléphone qui recevra l'SMS (avec le préfixe, ex: +33)
+		$aProviders = ['mms.bouyguestelecom.fr', 'sfr.fr', 'orange.fr', 'smtp.free.fr', 'pop.free.fr'];
+		$mailer = new \PHPMailer\PHPMailer\PHPMailer(true);
+		$mailer->IsSMTP(); // active SMTP
+		$mailer->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+		$mailer->SMTPAuth = true;  // Authentification SMTP active
+		$mailer->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
+		$mailer->Host = 'smtp.gmail.com';
+		$mailer->Port = 465;
+		$mailer->Username = 'nicolachoquet06250@gmail.com';
+		$mailer->Password = '1204NicolasChoquet2669!';
+		$mailer->SetFrom($mailer->Username, 'Pizzygo');
+		$mailer->Subject = '';
+		$mailer->Body = 'SMS';
+		foreach ($aProviders as $sProvider) {
+			$to = $sPhoneNum . '@' . $sProvider . '.com';
+			$mailer->AddAddress($to);
+		}
+		if(!$mailer->Send()) {
+			return 'Mail error: '.$mailer->ErrorInfo;
+		} else {
+			return true;
+		}
+//		foreach ($aProviders as $sProvider) {
+//			if(mail($sPhoneNum . '@' . $sProvider . '.com', '', 'Ce texto a été envoyé avec PHP, tout simplement !')) {
+//				echo 'envoyé'."\n";
+//				break;
+//			}
+//			else {
+//				echo 'pas envoyé'."\n";
+//				continue;
+//			}
+//		}
+	}
 }
--- extendable_classes/autoload.php
+++ extendable_classes/autoload.php
@@ -1,9 +1,20 @@
 <?php
 
 require_once __DIR__.'/Base.php';
+
+if(is_dir(__DIR__.'/PHPMailer')) {
+	require_once __DIR__.'/PHPMailer/src/POP3.php';
+	require_once __DIR__.'/PHPMailer/src/SMTP.php';
+	require_once __DIR__.'/PHPMailer/src/OAuth.php';
+	require_once __DIR__.'/PHPMailer/src/Exception.php';
+	require_once __DIR__.'/PHPMailer/src/PHPMailer.php';
+}
+
 $dir = opendir(__DIR__);
 while (($elem = readdir($dir)) !== false) {
 	if($elem !== '.' && $elem !== '..' && $elem !== 'autoload.php') {
-		require_once __DIR__.'/'.$elem;
+		if(is_file(__DIR__.'/'.$elem)) {
+			require_once __DIR__.'/'.$elem;
+		}
 	}
 }
