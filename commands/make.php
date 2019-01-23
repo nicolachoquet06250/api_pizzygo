cfe6256eeff2e245715ed19d1bbda1d324623bd4
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-01-23 17:02:37 +0100

    add demo and error controller

--- commands/make.php
+++ commands/make.php
@@ -8,41 +8,37 @@ class make extends cmd {
 	 * @throws \PHPMailer\PHPMailer\Exception
 	 */
 	protected function send_sms() {
-		$sPhoneNum = '+33763207630'; // Le numéro de téléphone qui recevra l'SMS (avec le préfixe, ex: +33)
-		$aProviders = ['mms.bouyguestelecom.fr', 'sfr.fr', 'orange.fr', 'smtp.free.fr', 'pop.free.fr'];
+		$sPhoneNum  = '+33763207630'; // Le numéro de téléphone qui recevra l'SMS (avec le préfixe, ex: +33)
+		$aProviders = ['mms.bouyguestelecom.fr', 'sms.bouyguestelecom.fr', 'sfr.fr', 'orange.fr', 'smtp.free.fr', 'pop.free.fr'];
 
-		$mailer = new PHPMailer(true);
+		$mailer = new PHPMailer();
+
 		$mailer->IsSMTP(); // active SMTP
-		$mailer->SMTPDebug = 1;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
-		$mailer->SMTPAuth = true;  // Authentification SMTP active
-		$mailer->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
+		$mailer->SMTPDebug  = 1;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+		$mailer->SMTPAuth   = true;  // Authentification SMTP active
+		$mailer->SMTPSecure = 'tls'; // Gmail REQUIERT Le transfert securise
+
 		$mailer->Host = 'smtp.gmail.com';
 		$mailer->Port = 587;
+
 		$mailer->Username = 'nicolachoquet06250@gmail.com';
-		$mailer->Password = '12042107NicolasChoquet2669!';
+		$mailer->Password = '1204210795NicolasChoquet2669!';
+
 		$mailer->SetFrom($mailer->Username, 'Pizzygo');
+
 		$mailer->Subject = 'SMS';
-		$mailer->Body = 'SMS';
+		$mailer->Body    = 'SMS';
+
 		$to = 'nicolachoquet06250@gmail.com';
 		$mailer->AddAddress($to);
-//		foreach ($aProviders as $sProvider) {
-//			$to = $sPhoneNum . '@' . $sProvider . '.com';
-//			$mailer->AddAddress($to);
-//		}
-		if(!$mailer->Send()) {
+		foreach ($aProviders as $sProvider) {
+			$to = $sPhoneNum.'@'.$sProvider;
+			$mailer->AddAddress($to);
+		}
+		if (!$mailer->Send()) {
 			return 'Mail error: '.$mailer->ErrorInfo;
 		} else {
 			return true;
 		}
-//		foreach ($aProviders as $sProvider) {
-//			if(mail($sPhoneNum . '@' . $sProvider . '.com', '', 'Ce texto a été envoyé avec PHP, tout simplement !')) {
-//				echo 'envoyé'."\n";
-//				break;
-//			}
-//			else {
-//				echo 'pas envoyé'."\n";
-//				continue;
-//			}
-//		}
 	}
 }
